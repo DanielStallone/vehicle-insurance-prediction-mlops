344961a1899a9fc75f48838b5e67dce28545b0c2
DanielStallone <stallone.daniel03@gmail.com>
2026-07-13 20:36:21 -0400

    initial setup complete

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Daniel S",
+    author_email="stallone.daniel03@gmail.com",
+    packages=find_packages()
+)
